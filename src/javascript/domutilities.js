872a2091992f997fb6ea620392e517ff71fd3418
Rabten Samphel <rabten@protonmail.com>
2022-02-16 20:45:29 -0500

    finished hamburger menu 🍔

--- src/javascript/domutilities.js
+++ src/javascript/domutilities.js
@@ -1,25 +1,30 @@
 const hamburg = document.getElementById('hamburger');
-const one = document.getElementById('hamburg1');
-const two = document.getElementById('hamburg2');
+const one = document.getElementById('lineOne');
+const two = document.getElementById('lineTwo');
+const three = document.getElementById('lineThree');
 const mobileMenu = document.getElementById('mobileMenu');
 const mobileContent = document.querySelector('.mobileContent');
 
-hamburg.addEventListener('click', () => {
+function animateHamburger() {
     if (hamburg.classList.contains('active')) {
         one.style.transform = 'rotate(45deg)';
-        one.style.marginBottom = '0'
-        two.style.transform = 'rotate(-45deg)';
+        two.style.display = 'none';
+        three.style.transform = 'rotate(-45deg)';
+        three.style.marginTop = '9.8px';
         hamburg.classList.remove('active');
-        mobileMenu.style.height = '90vh';
+        mobileMenu.style.height = '100vh';
         setTimeout(() => {
             mobileContent.style.display = 'flex';
         }, 1000);
     } else {
         one.style.transform = 'rotate(0deg)';
-        one.style.marginBottom = '1rem'
-        two.style.transform = 'rotate(0deg)';
+        two.style.display = 'block';
+        three.style.transform = 'rotate(0deg)';
+        three.style.marginTop = '0px';
         hamburg.classList.add('active');
         mobileMenu.style.height = '0';
         mobileContent.style.display = 'none';
     }
-})
+}
+
+hamburg.addEventListener('click', animateHamburger)
